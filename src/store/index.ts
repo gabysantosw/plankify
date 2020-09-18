030e3b874d106d3bffe99ae3bf370e0c31446a9f
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-18 15:58:18 +0200

    Set up store to handle planks

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,32 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+// import { Plank } from '@/types/index';
 
 Vue.use(Vuex);
 
+const SET_USER = 'set user';
+const ADD_PLANK = 'add plank';
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    username: 'Gaby',
+    planks: [{}],
+  },
+  mutations: {
+    [SET_USER](state, username) {
+      state.username = username;
+    },
+    [ADD_PLANK](state, plank) {
+      state.planks.push(plank);
+    },
+  },
+  actions: {
+    logUser({ commit }, username) {
+      commit(SET_USER, username);
+    },
+    addPlank({ commit }, plank) {
+      commit(ADD_PLANK, plank);
+    },
+  },
+  modules: {},
 });
