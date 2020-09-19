c9054d31ca79902b604b51d9126d891534252416
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-19 20:19:15 +0200

    Handle router links depending on current route name

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,12 @@
           router-link.logo(to="/") 
             span Plank
             span ify
-        li
+        li(v-if='this.$route.name === `Home`')
           router-link.nav-link(to="/progress") Progress
+        li(v-if='this.$route.name === `Planking`')
+          router-link.nav-link(to="/") Cancel
+        li(v-if='this.$route.name === `Progress`')
+          router-link.nav-link(to="/") Home
     router-view
 </template>
 
